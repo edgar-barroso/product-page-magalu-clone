b1b5122b822219aa546c52db40d481374039cf18
Jose Edgar Barroso Neto <edgarbarroso@MacBook-Air-de-Jose.local>
2024-12-30 01:12:04 -0300

    first commit

--- src/components/CarouselCustom.tsx
+++ src/components/CarouselCustom.tsx
@@ -3,10 +3,12 @@ import { Carousel } from "@material-tailwind/react";
 export function CarouselCustom() {
   return (
     <Carousel
+      placeholder={undefined}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
       autoplay
       autoplayDelay={5000}
       loop
-      
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="absolute bottom-0 left-2/4 z-50 flex -translate-x-2/4 gap-2">
           {new Array(length).fill("").map((_, i) => (
@@ -17,14 +19,13 @@ export function CarouselCustom() {
                   ? "h-2 w-2 bg-gray-700"
                   : "w-2 h-2 bg-gray-200"
               }`}
-            onClick={() => setActiveIndex(i)}
+              onClick={() => setActiveIndex(i)}
             />
           ))}
         </div>
       )}
       className="md:w-[40%]"
     >
-
       <img
         src="imagem2.webp"
         alt="image 2"
@@ -50,7 +51,7 @@ export function CarouselCustom() {
         alt="image 6"
         className="h-full w-fullobject-cover object-center md:object-contain"
       />
-            <img
+      <img
         src="imagem1.webp"
         alt="image 1"
         className="h-fullw-object-cover object-center md:object-contain"
